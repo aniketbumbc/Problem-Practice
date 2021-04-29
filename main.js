9153a7a78c14e701353fde5900ef5a5b53e7803a
aniket <aniket.umbc@gmail.com>
2021-04-29 07:01:21 +0530

    max char

--- main.js
+++ main.js
@@ -6,8 +6,29 @@
 //   return revsStr.join('');
 // };
 
-const reverse = (str) => {
-  return str.split('').reduce((rev, char) => char + rev, '');
-};
+// const reverse = (str) => {
+//   return str.split('').reduce((rev, char) => char + rev, '');
+// };
 
-console.log(reverse('abc'));
+function maxChar(str) {
+  let charObj = {};
+  let maxNum = 0;
+  let maxChar = '';
+
+  for (let char of str) {
+    if (!charObj[char]) {
+      charObj[char] = 1;
+    } else {
+      charObj[char]++;
+    }
+  }
+
+  for (let char in charObj) {
+    if (charObj[char] > maxNum) {
+      (maxNum = charObj[char]), (maxChar = char);
+    }
+  }
+  return maxChar + ' ' + maxNum;
+}
+//maxChar('Hellloe');
+console.log(maxChar('Hello there aniket'));
