30d81dcf75b0fe6a4150c569a53980f5a0305c4b
aniket <aniket.umbc@gmail.com>
2021-04-29 07:32:03 +0530

    fizz buzz

--- main.js
+++ main.js
@@ -10,25 +10,43 @@
 //   return str.split('').reduce((rev, char) => char + rev, '');
 // };
 
-function maxChar(str) {
-  let charObj = {};
-  let maxNum = 0;
-  let maxChar = '';
+// function maxChar(str) {
+//   let charObj = {};
+//   let maxNum = 0;
+//   let maxChar = '';
 
-  for (let char of str) {
-    if (!charObj[char]) {
-      charObj[char] = 1;
+//   for (let char of str) {
+//     if (!charObj[char]) {
+//       charObj[char] = 1;
+//     } else {
+//       charObj[char]++;
+//     }
+//   }
+
+//   for (let char in charObj) {
+//     if (charObj[char] > maxNum) {
+//       (maxNum = charObj[char]), (maxChar = char);
+//     }
+//   }
+//   return maxChar + ' ' + maxNum;
+// }
+// //maxChar('Hellloe');
+// console.log(maxChar('Hello there'));
+
+//Fizz buzz problem
+
+const fizzbuzz = (num) => {
+  for (let i = 1; i <= num; i++) {
+    if (i % 3 === 0 && i % 5) {
+      console.log('fizzbuzz');
+    } else if (i % 3 === 0) {
+      console.log('fizz');
+    } else if (i % 5 === 0) {
+      console.log('buzz');
     } else {
-      charObj[char]++;
+      console.log(i);
     }
   }
+};
 
-  for (let char in charObj) {
-    if (charObj[char] > maxNum) {
-      (maxNum = charObj[char]), (maxChar = char);
-    }
-  }
-  return maxChar + ' ' + maxNum;
-}
-//maxChar('Hellloe');
-console.log(maxChar('Hello there aniket'));
+fizzbuzz(15);
