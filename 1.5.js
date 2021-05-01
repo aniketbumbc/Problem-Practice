66910d36b678f05d4d0a1e86320c95fc5aa51754
aniket <aniket.umbc@gmail.com>
2021-05-01 13:10:44 +0530

    anagram sol 2

--- 1.5.js
+++ 1.5.js
@@ -1,30 +1,62 @@
-const str = 'Hello A Htere ';
-let newObj = {};
-
-for (let ele of str) {
-  if (newObj[ele] !== undefined) {
-    newObj[ele]++;
-  } else {
-    newObj[ele] = 1;
-  }
-}
-
-console.log(newObj);
-
 function findAnnagram(str1, str2) {
-  let splStr = str1.split('');
-  let splStr2 = str2.split('');
+  const charOccr1 = findOccurance(str1);
+  const charOccr2 = findOccurance(str2);
 
-  splStr.sort();
-  splStr2.sort();
-  if (splStr2.join('') === splStr.join('')) {
-    return true;
-  } else {
+  if (Object.keys(charOccr2).length !== Object.keys(charOccr1).length) {
     return false;
   }
+
+  for (let char in charOccr1) {
+    if (charOccr1[char] !== charOccr2[char]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function findOccurance(str) {
+  let tempObj = {};
+
+  for (let char of str) {
+    if (tempObj[char] === undefined) {
+      tempObj[char] = 1;
+    } else {
+      tempObj[char] += 1;
+    }
+  }
+  return tempObj;
 }
 
 console.log(findAnnagram('helloe', 'eollhe'));
+
+//
+
+// const str = 'Hello A Htere ';
+// let newObj = {};
+
+// for (let ele of str) {
+//   if (newObj[ele] !== undefined) {
+//     newObj[ele]++;
+//   } else {
+//     newObj[ele] = 1;
+//   }
+// }
+
+// console.log(newObj);
+
+// function findAnnagram(str1, str2) {
+//   let splStr = str1.split('');
+//   let splStr2 = str2.split('');
+
+//   splStr.sort();
+//   splStr2.sort();
+//   if (splStr2.join('') === splStr.join()) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
 
 // let numArr = [1, 3, 4, 5, 2];
 
--- dist/1.5.ff261ad6.js
+++ dist/1.5.ff261ad6.js
@@ -124,44 +124,71 @@ function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o =
 
 function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
 
-var str = 'Hello A Htere ';
-var newObj = {};
-
-var _iterator = _createForOfIteratorHelper(str),
-    _step;
-
-try {
-  for (_iterator.s(); !(_step = _iterator.n()).done;) {
-    var ele = _step.value;
-
-    if (newObj[ele] !== undefined) {
-      newObj[ele]++;
-    } else {
-      newObj[ele] = 1;
-    }
-  }
-} catch (err) {
-  _iterator.e(err);
-} finally {
-  _iterator.f();
-}
-
-console.log(newObj);
-
 function findAnnagram(str1, str2) {
-  var splStr = str1.split('');
-  var splStr2 = str2.split('');
-  splStr.sort();
-  splStr2.sort();
+  var charOccr1 = findOccurance(str1);
+  var charOccr2 = findOccurance(str2);
 
-  if (splStr2.join('') === splStr.join('')) {
-    return true;
-  } else {
+  if (Object.keys(charOccr2).length !== Object.keys(charOccr1).length) {
     return false;
   }
+
+  for (var char in charOccr1) {
+    if (charOccr1[char] !== charOccr2[char]) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
-console.log(findAnnagram('helloe', 'eollhe')); // let numArr = [1, 3, 4, 5, 2];
+function findOccurance(str) {
+  var tempObj = {};
+
+  var _iterator = _createForOfIteratorHelper(str),
+      _step;
+
+  try {
+    for (_iterator.s(); !(_step = _iterator.n()).done;) {
+      var char = _step.value;
+
+      if (tempObj[char] === undefined) {
+        tempObj[char] = 1;
+      } else {
+        tempObj[char] += 1;
+      }
+    }
+  } catch (err) {
+    _iterator.e(err);
+  } finally {
+    _iterator.f();
+  }
+
+  return tempObj;
+}
+
+console.log(findAnnagram('helloe', 'eollhe')); //
+// const str = 'Hello A Htere ';
+// let newObj = {};
+// for (let ele of str) {
+//   if (newObj[ele] !== undefined) {
+//     newObj[ele]++;
+//   } else {
+//     newObj[ele] = 1;
+//   }
+// }
+// console.log(newObj);
+// function findAnnagram(str1, str2) {
+//   let splStr = str1.split('');
+//   let splStr2 = str2.split('');
+//   splStr.sort();
+//   splStr2.sort();
+//   if (splStr2.join('') === splStr.join()) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+// let numArr = [1, 3, 4, 5, 2];
 // function chunckArr(numArr, chunkSize) {
 //   let startSize = 0;
 //   let finalChunk = [];
