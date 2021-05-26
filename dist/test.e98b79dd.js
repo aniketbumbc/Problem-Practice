1c51b3b86f7666becb1d2312b0bbf8b2b50ecc09
aniket <aniket.umbc@gmail.com>
2021-05-26 23:20:28 +0530

    currning

--- dist/test.e98b79dd.js
+++ dist/test.e98b79dd.js
@@ -124,6 +124,33 @@ console.log(aLet); // causes ReferenceError: aLet is not defined
 
 var aVar = 1;
 var aLet = 2;
+
+function outer() {
+  var a = 10;
+  var b = 20;
+
+  function inner() {
+    var c = 43;
+    console.log(b, aLet);
+  }
+
+  inner();
+}
+
+outer();
+
+function add(a) {
+  return function (b) {
+    return function (c) {
+      return function (d) {
+        return a + b + c + d;
+      };
+    };
+  };
+}
+
+var result = add(1)(2)(3)(4);
+console.log(result);
 },{}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
--- test.js
+++ test.js
@@ -2,3 +2,31 @@ console.log(aVar); // undefined
 console.log(aLet); // causes ReferenceError: aLet is not defined
 var aVar = 1;
 let aLet = 2;
+
+function outer() {
+  var a = 10;
+  var b = 20;
+
+  function inner() {
+    var c = 43;
+
+    console.log(b, aLet);
+  }
+
+  inner();
+}
+
+outer();
+
+function add(a) {
+  return (b) => {
+    return (c) => {
+      return (d) => {
+        return a + b + c + d;
+      };
+    };
+  };
+}
+
+const result = add(1)(2)(3)(4);
+console.log(result);
