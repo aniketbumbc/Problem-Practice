411a0cbee30dadf4c9b4b6c8fd0de971adbb14fc
aniket <aniket.umbc@gmail.com>
2021-06-12 16:40:08 +0530

    code

--- Others/testq.js
+++ Others/testq.js
@@ -8,18 +8,91 @@ function multiply(a) {
 
 multiply(5)(6);
 
+// let obj = {
+//   x: 1,
+
+//   getX() {
+//     function inner() {
+//       console.log(this.x);
+//     }
+
+//     inner();
+//   },
+// };
+
+// let test = obj.getX();
+
+// obj.inner.bind(obj);
+
+function abc() {
+  try {
+    throw new Error();
+  } catch (x) {
+    var x = 1;
+    y = 2;
+    console.log(x); // only for this block
+  }
+  console.log(x); // hoisted at top which undefined
+  console.log(y);
+}
+
+abc();
+
+var x = 21;
+var girl = function () {
+  console.log(x);
+  var x = 20;
+};
+girl();
+
+var h = 21000;
+function boy() {
+  console.log('dd', h);
+  var h = 1000;
+}
+boy();
+
+var b = 1;
+function outer() {
+  var b = 2;
+  function inner() {
+    b++;
+    var c = 30;
+    console.log(b);
+  }
+  inner();
+  console.log(b);
+}
+outer();
+
 let obj = {
-  x: 1,
-
-  getX() {
-    function inner() {
-      console.log(this.x);
-    }
-
-    inner();
+  a: 1,
+  b: 2,
+  c: {
+    age: 30,
   },
 };
 
-let test = obj.getX();
+var objclone = Object.assign({}, obj);
+console.log('objclone: ', objclone);
+obj.c.age = 45;
 
-obj.bind(inner());
+function foo() {
+  console.log(1);
+}
+
+//alert(foo()); // what will this alert?
+
+foo();
+
+function foo() {
+  console.log(2);
+}
+
+for (var i = 0; i < 5; i++) {
+  (function (i) {
+    setTimeout(() => {
+      console.log(i);
+    }, i * 1000);
+  })(i);
+}
