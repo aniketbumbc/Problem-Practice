9b3375a843b82b50569fcee6794ccd55c85f007c
aniket <aniket.umbc@gmail.com>
2021-06-23 06:55:55 +0530

    array freq new technique

--- Others/DS/Practice Pattern/index 23.6.js
+++ Others/DS/Practice Pattern/index 23.6.js
@@ -19,6 +19,44 @@ function checkFreq(arr1, arr2) {
   return true;
 }
 
-// console.log();
+// New Technique
 
-checkFreq([1, 3, 4], [14, 9, 1]);
+function isCheckFrequency(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  let obj1 = {};
+  let obj2 = {};
+
+  for (let value of arr1) {
+    if (obj1[value] === undefined) {
+      obj1[value] = 1;
+    } else {
+      obj1[value] += 1;
+    }
+  }
+
+  for (let value of arr2) {
+    if (obj2[value] === undefined) {
+      obj2[value] = 1;
+    } else {
+      obj2[value] += 1;
+    }
+  }
+
+  for (let value in obj1) {
+    if (!(value ** 2 in obj2)) {
+      return false;
+    }
+
+    let squre = value * value;
+    if (obj2[squre] !== obj1[value]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+console.log(isCheckFrequency([1, 3, 4, 3], [16, 9, 1, 9]));
--- dist/index 23.6.5d9c2ec1.js
+++ dist/index 23.6.5d9c2ec1.js
@@ -118,6 +118,12 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   return newRequire;
 })({"Others/DS/Practice Pattern/index 23.6.js":[function(require,module,exports) {
+function _createForOfIteratorHelper(o, allowArrayLike) { var it = typeof Symbol !== "undefined" && o[Symbol.iterator] || o["@@iterator"]; if (!it) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = it.call(o); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it.return != null) it.return(); } finally { if (didErr) throw err; } } }; }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
 /** Check Frequency Same of array squre */
 function checkFreq(arr1, arr2) {
   if (arr1.length !== arr2.length) {
@@ -136,10 +142,71 @@ function checkFreq(arr1, arr2) {
   }
 
   return true;
-} // console.log();
+} // New Technique
 
 
-checkFreq([1, 3, 4], [14, 9, 1]);
+function isCheckFrequency(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  var obj1 = {};
+  var obj2 = {};
+
+  var _iterator = _createForOfIteratorHelper(arr1),
+      _step;
+
+  try {
+    for (_iterator.s(); !(_step = _iterator.n()).done;) {
+      var _value = _step.value;
+
+      if (obj1[_value] === undefined) {
+        obj1[_value] = 1;
+      } else {
+        obj1[_value] += 1;
+      }
+    }
+  } catch (err) {
+    _iterator.e(err);
+  } finally {
+    _iterator.f();
+  }
+
+  var _iterator2 = _createForOfIteratorHelper(arr2),
+      _step2;
+
+  try {
+    for (_iterator2.s(); !(_step2 = _iterator2.n()).done;) {
+      var _value2 = _step2.value;
+
+      if (obj2[_value2] === undefined) {
+        obj2[_value2] = 1;
+      } else {
+        obj2[_value2] += 1;
+      }
+    }
+  } catch (err) {
+    _iterator2.e(err);
+  } finally {
+    _iterator2.f();
+  }
+
+  for (var value in obj1) {
+    if (!(Math.pow(value, 2) in obj2)) {
+      return false;
+    }
+
+    var squre = value * value;
+
+    if (obj2[squre] !== obj1[value]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+console.log(isCheckFrequency([1, 3, 4, 3], [16, 9, 1, 9]));
 },{}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
