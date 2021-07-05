070edfe3e36491e6fe1eb05dbf0730e9e0889c9d
aniket <aniket.umbc@gmail.com>
2021-07-05 07:44:39 +0530

    recursive practice - 2

--- Code/DS/Practice Pattern/index 05.07.js
+++ Code/DS/Practice Pattern/index 05.07.js
@@ -13,7 +13,7 @@ productArray([1, 2, 3, 10]);
 // recursive way to product array
 
 function prouctRecursive(arr) {
-  console.log(arr);
+  //   console.log(arr);
   if (arr.length === 0) {
     return 1;
   }
@@ -21,4 +21,27 @@ function prouctRecursive(arr) {
   return arr[0] * prouctRecursive(arr.slice(1));
 }
 
-console.log(prouctRecursive([1, 2, 3, 4, 5, 6]));
+// console.log(prouctRecursive([1, 2, 3, 4, 5, 6]));
+
+// recursive way to for range
+
+function recursiveRange(num) {
+  if (num === 0) {
+    return 0;
+  }
+
+  return num + recursiveRange(num - 1);
+}
+
+// console.log(recursiveRange(10));
+
+// fibo series
+
+function fiboSeries(n) {
+  if (n <= 2) {
+    return 1;
+  }
+  return fiboSeries(n - 1) + fiboSeries(n - 2);
+}
+
+console.log(fiboSeries(8));
