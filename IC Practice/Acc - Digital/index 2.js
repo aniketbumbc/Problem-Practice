6664a7e645940836ad8436a6bd106344cf526ff2
aniket <aniket.umbc@gmail.com>
2021-07-26 22:31:09 +0530

    comment move

--- IC Practice/Acc - Digital/index 2.js
+++ IC Practice/Acc - Digital/index 2.js
@@ -4,7 +4,8 @@ var x = 23;
 var number = [1, 4, 5, 3, 5];
 var num = number;
 num.push(34);
-console.log(number, num); //[1, 4, 5, 3, 5,34] [1, 4, 5, 3, 5,34]
+console.log(number, num);
+//[1, 4, 5, 3, 5,34] [1, 4, 5, 3, 5,34]
 
 var str = 'A' + 'B' + +'.' + 'B'.toLocaleLowerCase();
 
